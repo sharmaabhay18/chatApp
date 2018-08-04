c25d68f7ea5465494ba5c31da53c15181b30e333
sharmaabhay18 <sharmaabhay47@gmail.com>
2018-08-04 14:32:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,15 +11,7 @@ import RouterComponent from './RouterComponent';
 class App extends Component {
 
  componentWillMount() {
-   const config = {
-      apiKey: 'AIzaSyADD4Z51xRzI1nWMC0hfP8W6IUNaCMEhQs',
-      authDomain: 'chatapp-81f14.firebaseapp.com',
-      databaseURL: 'https://chatapp-81f14.firebaseio.com',
-      projectId: 'chatapp-81f14',
-      storageBucket: 'chatapp-81f14.appspot.com',
-      messagingSenderId: '30449396221'
-    };
-    firebase.initializeApp(config);
+ 
  }
 
   render() {
